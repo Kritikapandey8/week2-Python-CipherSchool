1f8629c00e8a602a118a1aad5725ef5022ca911f
Kritikapandey8 <115393154+Kritikapandey8@users.noreply.github.com>
2022-12-16 01:10:36 +0530

    Add files via upload

---
+++ Que42.py
@@ -0,0 +1,12 @@
+# update method
+
+user_info={
+    'name':'Kritika',
+    'age': 18,
+    'fav_movies': ['coco'],
+    'fav_tunes': ['awakening','fairy tale'],
+}
+
+more_info= {'state' : 'Madhya Pradesh', 'hobbies' : ['coding','reading','Playing']}
+user_info.update({})
+print(user_info)
---
+++ Que43.py
@@ -0,0 +1,4 @@
+# more about get method
+user= {'name' : 'Kritika', 'age' : '18', 'age' : '19'}
+print(user.get('names','not found'))
+print(user)
